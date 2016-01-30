b65b66074ea6ea2f4868d3602e6b985ddab0f25f
George F Rogers <grogers385@gmail.com>
2016-01-30 13:49:55 -0600

    update

---
+++ essocony.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/agent-pink/esso"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.Handle("/", esso.App)
+	fmt.Println("Listening on port:", port)
+	http.ListenAndServe(":"+port, nil)
+}
